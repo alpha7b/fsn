64fc7988f92d94ebb9bfdacbb43eda435efaca3d
alpha7b <55778604+alpha7b@users.noreply.github.com>
2020-07-30 09:18:27 +0800

    Create setup_swapbot.sh

---
+++ setup_swapbot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# install node & pm2
+sudo apt update
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt install nodejs -y
+sudo npm i -g pm2
+
+# download swapbot code
+git clone https://github.com/fusionfans/swapbot.git
+
+# config bot
+cd swapbot
+cp config.sample.js config.js
+
+read -p "Please paste your private key: " priKey
+sed -i "s/your private key/$priKey/g" config.js
+
+sudo npm i
+
+# start bot
+sudo nmp run start
